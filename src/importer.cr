d9ce5f9fbdb1de7d780b3ac6a9dc551a6fb1b980
wongherlung <wongherlaang@estl.sg>
2020-12-12 22:18:38 +0800

    Added home dir for cashflow calculator

--- src/importer.cr
+++ src/importer.cr
@@ -1,23 +1,38 @@
+HOME_DIR_NAME = ".cashflow-calculator"
+
 class Importer
-  property csv_files
+  property csv_files, account_files, category_files
 
   def initialize
     @csv_files = Array(String).new
+    @account_files = Array(String).new
+    @category_files = Array(String).new
   end
 
   def import
     validate
+
+    # Create local folder if already does not exist
+    home_path = File.expand_path("~/", home: true)
+    unless Dir.exists?("#{home_path}/#{HOME_DIR_NAME}")
+      Dir.mkdir("#{home_path}/#{HOME_DIR_NAME}")
+      Dir.mkdir("#{home_path}/#{HOME_DIR_NAME}/categories")
+      Dir.mkdir("#{home_path}/#{HOME_DIR_NAME}/accounts")
+    end
 
     @csv_files = if ARGV.size == 1
                    Dir["#{ARGV[0]}/*.csv"]
                  else
                    ["./"]
                  end
+
+    @categories_files = Dir["#{home_path}/#{HOME_DIR_NAME}/categories/*.yml"]
+    @account_files = Dir["#{home_path}/#{HOME_DIR_NAME}/accounts/*.yml"]
   end
 
   private def validate
     if ARGV.size == 1
-      # Checks if provided direcotry exists or not
+      # Checks if provided directory exists or not
       unless Dir.exists?(ARGV[0])
         STDERR.puts("[Error] The provided directory does not exist.")
         exit

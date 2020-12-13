71306b42bb05eb10fa6b40b19a27ce1f60cc12aa
wongherlung <wongherlaang@estl.sg>
2020-12-13 10:38:28 +0800

    Add messages

--- src/main.cr
+++ src/main.cr
@@ -7,11 +7,15 @@ def main
   importer = Importer.new
   importer.import
 
+  puts("Parsing #{importer.csv_files.size} CSV files.")
+  puts("Parsing #{importer.account_files.size} account YAML files.")
+  puts("Parsing #{importer.category_files.size} category YAML files.")
   parsing_engine = ParsingEngine.new(importer.csv_files,
                                      importer.account_files,
                                      importer.category_files)
   transactions = parsing_engine.parse
 
+  puts("Calculating expenses.")
   calculation_engine = CalculationEngine.new(transactions)
   calculation_engine.display_summary(TransactionType::Inflow)
   calculation_engine.display_summary(TransactionType::Outflow)

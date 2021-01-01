0d5d4682543a502c19460977e11c7b0aa8bf2736
wongherlung <wongherlaang@estl.sg>
2021-01-01 16:35:36 +0800

    Allow for exporting inflow/outflow totals

--- src/exporter.cr
+++ src/exporter.cr
@@ -15,10 +15,16 @@ class Exporter
     return row if matches.empty?
 
     matches.each do |m|
-      key = m[0].match(/[^>^<]+/).not_nil!
-      descriptions = @calculation_engine.get_descriptions_for_transactions(key[0]) if show_descriptions
-      row = row.gsub(m[0], "#{@calculation_engine.total(nil, key[0])},\"#{descriptions}\"") if show_descriptions
-      row = row.gsub(m[0], @calculation_engine.total(nil, key[0])) unless show_descriptions
+      key = m[0].match(/[^>^<]+/).not_nil![0]
+      transaction_type = nil
+      if key.split(":").size > 1
+        transaction_type = TransactionType::Outflow if key.split(":").last == "outflow"
+        transaction_type = TransactionType::Inflow if key.split(":").last == "inflow"
+        key = key.split(":").first
+      end
+      descriptions = @calculation_engine.get_descriptions_for_transactions(key) if show_descriptions
+      row = row.gsub(m[0], "#{@calculation_engine.total(transaction_type, key)},\"#{descriptions}\"") if show_descriptions
+      row = row.gsub(m[0], @calculation_engine.total(transaction_type, key)) unless show_descriptions
     end
     return row
   end

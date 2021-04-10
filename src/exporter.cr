19920726de03f2c8f35a73a0d19649ef40f7bfaf
wongherlung <wongherlung@gmail.com>
2021-04-10 13:07:14 +0800

    Make output.csv friendly for google sheets import

--- src/exporter.cr
+++ src/exporter.cr
@@ -3,11 +3,13 @@ class Exporter
   end
 
   def export_csv_report(output_file : String, show_descriptions : Bool)
-    rows = File.read_lines(output_file)
+    rows = File.read_lines(output_file).clone()
+    description_rows = rows.clone()
     rows = rows.map { |i| replace_with_total(i, show_descriptions) }
 
     rows.push("\"#{@calculation_engine.transactions_above(TransactionType::Outflow, 100.00).map { |t| t.description + " - $" + t.value.to_s }.join("\n")}\"")
-    File.write("./output.csv", rows.join("\n"))
+    File.write("./output.csv", rows.join("\n")) unless show_descriptions
+    File.write("./output.csv", rows.join("\n") + get_descriptions(description_rows).join("\n")) if show_descriptions
   end
 
   private def replace_with_total(row : String, show_descriptions : Bool) : String
@@ -22,10 +24,27 @@ class Exporter
         transaction_type = TransactionType::Inflow if key.split(":").last == "inflow"
         key = key.split(":").first
       end
-      descriptions = @calculation_engine.get_descriptions_for_transactions(key) if show_descriptions
-      row = row.gsub(m[0], "#{@calculation_engine.total(transaction_type, key)},\"#{descriptions}\"") if show_descriptions
-      row = row.gsub(m[0], @calculation_engine.total(transaction_type, key)) unless show_descriptions
+      row = row.gsub(m[0], @calculation_engine.total(transaction_type, key))
     end
     return row
+  end
+
+  private def get_descriptions(rows : Array(String)) : Array(String)
+    descriptions = [] of String
+
+    rows.each do |row|
+      matches = row.scan(/<[^,]*>/)
+      next if matches.empty?
+
+      matches.each do |m|
+        key = m[0].match(/[^>^<]+/).not_nil![0]
+        key = key.split(":").first
+
+        descriptions.push(@calculation_engine.get_descriptions_for_transactions(key))
+      end
+    end
+
+    
+    return descriptions
   end
 end
